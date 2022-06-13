d2d2ea9cc5f6cfaf92856a0e85da58109c33291d
ywalakamar <barxwells@gmail.com>
2022-06-13 19:09:48 +0300

    chore(tdd): test create product method
    - test `POST /products` endpoint

--- src/main/java/com/ywalakamar/crud/model/Product.java
+++ src/main/java/com/ywalakamar/crud/model/Product.java
@@ -21,4 +21,11 @@ public class Product {
     private String name;
     private int quantity;
     private double price;
+
+    public Product(String name, int quantity, double price) {
+        this.name = name;
+        this.quantity = quantity;
+        this.price = price;
+    }
+
 }
--- src/test/java/com/ywalakamar/crud/CrudApplicationTests.java
+++ src/test/java/com/ywalakamar/crud/CrudApplicationTests.java
@@ -1,13 +1,39 @@
 package com.ywalakamar.crud;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.web.client.RestTemplate;
 
-@SpringBootTest
+import com.ywalakamar.crud.model.Product;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CrudApplicationTests {
 
+	@LocalServerPort
+	private int port;
+	private String baseUrl = "http://localhost";
+	private static RestTemplate rest;
+
+	@BeforeAll
+	public static void init() {
+		rest = new RestTemplate();
+	}
+
+	@BeforeEach
+	public void setUp() {
+		baseUrl = baseUrl.concat(":").concat(port + "").concat("/products");
+	}
+
 	@Test
-	void contextLoads() {
+	void testCreateProduct() {
+		Product product = new Product("Earphones", 2, 700.00);
+		Product response = rest.postForObject(baseUrl, product, Product.class);
+		assertEquals("Earphones", response.getName());
 	}
 
 }
