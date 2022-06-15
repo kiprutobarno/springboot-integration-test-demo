3416d30b08c4f7d137e3fa6da56cb467a3b1b6dd
ywalakamar <barxwells@gmail.com>
2022-06-15 13:37:12 +0300

    feat(product): update product
    - create `/product/update/id` endpoint

--- src/main/java/com/ywalakamar/crud/controller/ProductController.java
+++ src/main/java/com/ywalakamar/crud/controller/ProductController.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -31,7 +33,12 @@ public class ProductController {
     }
 
     @GetMapping("/name")
-    public Product findProductByName(@RequestParam("name") String name){
+    public Product findProductByName(@RequestParam("name") String name) {
         return service.getProductByName(name);
+    }
+
+    @PutMapping("/update/{id}")
+    public Product updateProduct(@RequestBody Product product, @PathVariable int id) {
+        return service.updateProduct(id, product);
     }
 }
